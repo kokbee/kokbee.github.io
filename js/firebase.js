4da5cd613b7bb26bf3d8814ca8c83394eb91c2ec
idea <idea@localhost.localdomain>
2021-03-03 12:19:20 +0900

    update index, edit, db.js

---
+++ js/firebase.js
@@ -0,0 +1,42 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBPPs2vC1vuZV3uQKSNWyAqEelZwR2NT0U",
+    authDomain: "githubio-b4a77.firebaseapp.com",
+    databaseURL: "https://githubio-b4a77-default-rtdb.firebaseio.com",
+    projectId: "githubio-b4a77",
+    storageBucket: "githubio-b4a77.appspot.com",
+    messagingSenderId: "243003904340",
+    appId: "1:243003904340:web:abd4b6f3305fa9953cb8c1",
+    measurementId: "G-ZZDP4QSF3R"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+function boardWhite(){
+    let fb= firebase.database()
+
+
+
+    fb.ref('board/projects').set({
+        project:project,
+        ts:ts,
+        technical:technical,
+        during:during,
+        author:author,
+        work:work,
+        team:team,
+        text:text,
+        description:description,
+        issue:issue,
+        url:url,
+        tag:tag,
+    });
+}
+
+function boardRead(){
+    let fb= firebase.database()
+    fb.ref("board/projects").on("value", function(data){
+        console.log(data.val())
+    })
+}
